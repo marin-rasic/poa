7548b7e8e1b5e0b284610ee8f10a6faf2d214d97
Marin Rašić <mrasic123@gmail.com>
2021-05-22 22:17:48 +0200

    Parallelized the processing of input file.

--- src/poa.cpp
+++ src/poa.cpp
@@ -6,20 +6,20 @@
 
 #include "alignment.hpp"
 #include "bioparser/fasta_parser.hpp"
+#include "omp.h"
 
-static struct option long_options[] = {
-    {"help", no_argument, NULL, 'h'},
-    {"match", required_argument, NULL, 'm'},
-    {"alignment", required_argument, NULL, 'a'},
-    {"mismatch", required_argument, NULL, 'n'},
-    {"gap", required_argument, NULL, 'g'}};
+static struct option long_options[] = {{"help", no_argument, NULL, 'h'},
+                                       {"match", required_argument, NULL, 'm'},
+                                       {"alignment", required_argument, NULL, 'a'},
+                                       {"mismatch", required_argument, NULL, 'n'},
+                                       {"gap", required_argument, NULL, 'g'},
+                                       {"threads", required_argument, NULL, 't'}};
 
 class Sequence {
    public:
     std::string name;
     std::string data;
-    Sequence(const char *name, std::uint32_t name_len, const char *data, std::uint32_t data_len)
-        : name(name, name_len), data(data, data_len){};
+    Sequence(const char *name, std::uint32_t name_len, const char *data, std::uint32_t data_len) : name(name, name_len), data(data, data_len){};
 };
 
 void Help() {
@@ -41,6 +41,9 @@ void Help() {
                  "  -g, --gap <int>\n"
                  "      default: -1\n"
                  "      linear gap penalty\n"
+                 "  -t, --threads <int>\n"
+                 "      default: 1\n"
+                 "      number of threads"
                  "  -h, --help\n"
                  "      Print usage information\n";
 }
@@ -52,8 +55,9 @@ int main(int argc, char *argv[]) {
     int8_t mismatch = -1;
     int8_t gap = -1;
     int8_t alignment = 0;
+    int8_t num_of_threads = 1;
 
-    while ((opt = getopt_long(argc, argv, "a:g:hm:n:", long_options, NULL)) != -1) {
+    while ((opt = getopt_long(argc, argv, "a:g:hm:n:t:", long_options, NULL)) != -1) {
         switch (opt) {
             case 'a':
                 alignment = atoi(optarg);
@@ -70,6 +74,9 @@ int main(int argc, char *argv[]) {
             case 'n':
                 mismatch = atoi(optarg);
                 break;
+            case 't':
+                num_of_threads = atoi(optarg) > omp_get_max_threads() ? omp_get_max_threads() : atoi(optarg);
+                break;
             default:
                 return 1;
         }
@@ -83,21 +90,52 @@ int main(int argc, char *argv[]) {
     std::string sequence_file = argv[optind];
     auto sequences = bioparser::Parser<Sequence>::Create<bioparser::FastaParser>(sequence_file)->Parse(-1);
 
-    for (int i = 0; i < sequences.size(); i++) {
-        std::cout << sequences[i]->name << std::endl;
-        std::cout << sequences[i]->data.size() << std::endl;
-    }
     Aligner *a = new Aligner(match, mismatch, gap, (Alignment)alignment);
-    Graph g;
-    a->AlignAndGraphTwoSeq(g, sequences[0]->data.c_str(), sequences[0]->data.size(), sequences[0]->name.c_str(),
-                           sequences[2]->data.c_str(), sequences[2]->data.size(), sequences[2]->name.c_str());
 
-    for (Node *node : g.start_nodes) {
-        std::cout << node->letter << ", ";
+    Graph graphs[num_of_threads];
+    int index = sequences.size() / num_of_threads;
+
+    std::vector<int> used_index(num_of_threads);
+
+    for (int i = 0; i < num_of_threads; i++) {
+        LinearGraph(graphs[i],
+                    sequences[i * index]->data.c_str(),
+                    sequences[i * index]->data.size(),
+                    sequences[i * index]->name.c_str());
+        used_index.push_back(i * index);
+    }
+
+#pragma omp parallel for schedule(static) num_threads(num_of_threads)
+    for (int i = 0; i < sequences.size(); i++) {
+        bool used = false;
+        for (int index : used_index) {
+            if (i == index) {
+                used = true;
+            }
+        }
+
+        if (used) {
+            continue;
+        }
+
+        a->AlignAndGraphSeqAndGraph(graphs[omp_get_thread_num()],
+                                    sequences[i]->data.c_str(),
+                                    sequences[i]->data.size(),
+                                    sequences[i]->name.c_str());
+    }
+
+    for (int i = 1; i < num_of_threads; i++) {
+        a->AlignAndGraphTwoGraph(graphs[0], graphs[i]);
+    }
+
+    std::vector<Node *> graph = graphs[0].TopologicalSort();
+    for (Node *node : graph) {
+        std::cout << node->letter << " ";
         for (auto origin : node->origin_of_letter) {
-            std::cout << std::get<0>(origin) << ", ";
+            std::cout << std::get<0>(origin) << std::get<1>(origin) << " ";
         }
         std::cout << std::endl;
     }
+    std::cout << std::endl;
     return 0;
 }
